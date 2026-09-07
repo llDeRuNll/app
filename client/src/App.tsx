8dbeb4a6c246b193b549b591b267d9f09c9c705f
llDeRuNll <eduardprudiys@gmail.com>
2026-09-07 18:37:10 +0200

    try

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import WelcomePage from "./pages/WelcomePage/WelcomePage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <WelcomePage />
+    </>
+  );
 }
 
 export default App;
