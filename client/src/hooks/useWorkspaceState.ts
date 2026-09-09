7f28d4441da7eeade08c048b187f1ed5482051c1
llDeRuNll <eduardprudiys@gmail.com>
2026-09-09 20:16:49 +0200

    allowed add Board and Task

--- client/src/hooks/useWorkspaceState.ts
+++ client/src/hooks/useWorkspaceState.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { mockData } from "../data/mockData";
-import type { WorkspaceData, Workspace } from "../types/workspace";
+import type { WorkspaceData, Workspace, Board, Task } from "../types/workspace";
 
 const useWorkspaceState = () => {
   const [data, setData] = useState<WorkspaceData>(mockData);
@@ -16,9 +16,57 @@ const useWorkspaceState = () => {
       workspaces: [...prevData.workspaces, newWorkspace],
     }));
   };
+
+  const addBoard = (workspaceId: string, name: string) => {
+    const newBoard: Board = {
+      id: crypto.randomUUID(),
+      name,
+      tasks: [],
+    };
+    setData((prevData) => ({
+      ...prevData,
+      workspaces: prevData.workspaces.map((workspace) =>
+        workspace.id === workspaceId
+          ? {
+              ...workspace,
+              boards: [...workspace.boards, newBoard],
+            }
+          : workspace,
+      ),
+    }));
+  };
+  const addTask = (workspaceId: string, boardId: string, title: string) => {
+    const newTask: Task = {
+      id: crypto.randomUUID(),
+      title,
+    };
+
+    setData((prevData) => ({
+      ...prevData,
+
+      workspaces: prevData.workspaces.map((workspace) =>
+        workspace.id === workspaceId
+          ? {
+              ...workspace,
+
+              boards: workspace.boards.map((board) =>
+                board.id === boardId
+                  ? {
+                      ...board,
+                      tasks: [...board.tasks, newTask],
+                    }
+                  : board,
+              ),
+            }
+          : workspace,
+      ),
+    }));
+  };
   return {
     data,
     addWorkspace,
+    addBoard,
+    addTask,
   };
 };
 export default useWorkspaceState;
