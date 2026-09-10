e287f5bb66f981a7836da7569c28705e7b3b106e
llDeRuNll <eduardprudiys@gmail.com>
2026-09-10 21:41:12 +0200

    implemented CRUD for workspaces and columns

--- client/src/hooks/useWorkspaceState.ts
+++ client/src/hooks/useWorkspaceState.ts
@@ -62,11 +62,70 @@ const useWorkspaceState = () => {
       ),
     }));
   };
+  const editWorkspace = (workspaceId: string, newName: string) => {
+    setData((prevData) => ({
+      ...prevData,
+      workspaces: prevData.workspaces.map((workspace) =>
+        workspace.id === workspaceId
+          ? {
+              ...workspace,
+              name: newName,
+            }
+          : workspace,
+      ),
+    }));
+  };
+  const deleteWorkspace = (workspaceId: string) => {
+    setData((prevData) => ({
+      ...prevData,
+      workspaces: prevData.workspaces.filter(
+        (workspace) => workspace.id !== workspaceId,
+      ),
+    }));
+  };
+
+  const editBoard = (workspaceId: string, boardId: string, newName: string) => {
+    setData((prevData) => ({
+      ...prevData,
+      workspaces: prevData.workspaces.map((workspace) =>
+        workspace.id === workspaceId
+          ? {
+              ...workspace,
+              boards: workspace.boards.map((board) =>
+                board.id === boardId
+                  ? {
+                      ...board,
+                      name: newName,
+                    }
+                  : board,
+              ),
+            }
+          : workspace,
+      ),
+    }));
+  };
+  const deleteBoard = (workspaceId: string, boardId: string) => {
+    setData((prevData) => ({
+      ...prevData,
+      workspaces: prevData.workspaces.map((workspace) =>
+        workspace.id === workspaceId
+          ? {
+              ...workspace,
+              boards: workspace.boards.filter((board) => board.id !== boardId),
+            }
+          : workspace,
+      ),
+    }));
+  };
   return {
     data,
     addWorkspace,
     addBoard,
     addTask,
+    editWorkspace,
+    deleteWorkspace,
+    editBoard,
+    deleteBoard,
   };
 };
 export default useWorkspaceState;
